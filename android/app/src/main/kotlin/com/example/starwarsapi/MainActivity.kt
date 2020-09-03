2b6bbd95a6354085fa91fed71c81a5073f85b108
rubensmk <mititaka@gmail.com>
2020-09-02 21:36:17 -0300

    model commit

---
+++ android/app/src/main/kotlin/com/example/starwarsapi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.starwarsapi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Star Wars API"),
+      ),
+      body: Center(
+        child: TextField(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
